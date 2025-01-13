6275a103810f758c77efd0f68678fb6641b4a37b
jaeukkang12 <kangjaeuk0312@naver.com>
2025-01-13 20:57:54 +0900

    :memo: Add: NumberUtil.isNumber, NumberUtil.format(double, float)

--- src/main/java/com/github/jaeukkang12/lib/util/NumberUtil.java
+++ src/main/java/com/github/jaeukkang12/lib/util/NumberUtil.java
@@ -3,7 +3,31 @@ package com.github.jaeukkang12.lib.util;
 import java.text.DecimalFormat;
 
 public class NumberUtil {
+    public static boolean isNumber(double d) {
+        return d == (int) d;
+    }
+
+    public static boolean isNumber(float f) {
+        return f == (int) f;
+    }
+
     public static String format(int i) {
         return new DecimalFormat("###,###").format(i);
+    }
+
+    public static String format(double d) {
+        if (isNumber(d)) {
+            return new DecimalFormat("###,###").format(d);
+        } else {
+            throw new RuntimeException();
+        }
+    }
+
+    public static String format(float f) {
+        if (isNumber(f)) {
+            return new DecimalFormat("###,###").format(f);
+        } else {
+            throw new RuntimeException();
+        }
     }
 }
